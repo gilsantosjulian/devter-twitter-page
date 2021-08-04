6320d10cc5221204794b712b5023fb5590e6f1dc
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-08-03 20:31:53 -0500

    feat: add getInitialProps function in timeline page

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -1,13 +1,19 @@
 import styles from './index.module.css'
 import Link from 'next/link'
 
-const Timeline = () => {
+const Timeline = ({ userName }) => {
   return (
     <>
-      <h1 className={styles.headline}>This is the timeline</h1>
+      <h1 className={styles.headline}>This is the timeline of {userName}</h1>
       <Link href="/">Go Home</Link>
     </>
   )
+}
+
+Timeline.getInitialProps = () => { // could be a promise
+  const url = 'http://localhost:3000/api/hello'
+  return fetch(url)
+    .then(res => res.json())
 }
 
 export default Timeline
