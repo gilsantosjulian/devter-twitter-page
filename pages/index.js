5c2b3ee0e51508d9296bc3ba7585b82b43687965
gilsantosjulian <gilsantosjulian@gmail.com>
2021-09-22 18:57:53 -0500

    fix: button styles

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ const Home = () => {
           <div>
             <Button onClick={() => {}}>
               <GitHub width={25} height={25} fill={'#fff'}/>
-              Login with github
+              <span>Login with github</span>
             </Button>
           </div>
         </section>
