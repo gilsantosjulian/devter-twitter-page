d7eac33ce79304a243e8d302bdbb8b92c98548bd
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-10-01 22:02:03 -0500

    chore: format conde - components

--- components/AppLayout/styles.js
+++ components/AppLayout/styles.js
@@ -1,21 +1,20 @@
-import css from 'styled-jsx/css'
+import css from "styled-jsx/css";
 
-import { breakpoints, colors, fonts } from '../../styles/theme'
-import { addOpacityToColor } from '../../styles/utils'
+import { breakpoints, colors, fonts } from "../../styles/theme";
+import { addOpacityToColor } from "../../styles/utils";
 
-const backgroundColor = addOpacityToColor(colors.primary, 0.3)
+const backgroundColor = addOpacityToColor(colors.primary, 0.3);
 
 export const globalStyles = css.global`
   html,
   body {
-    background-image:
-      radial-gradient(${backgroundColor} 1px, #fdfdfd 1px),
+    background-image: radial-gradient(${backgroundColor} 1px, #fdfdfd 1px),
       radial-gradient(${backgroundColor} 1px, #fdfdfd 1px);
     background-position: 0 0, 25px 25px;
     background-size: 50px 50px;
     padding: 0;
     margin: 0;
-    font-family: ${fonts.base}
+    font-family: ${fonts.base};
   }
 
   a {
@@ -26,7 +25,7 @@ export const globalStyles = css.global`
   * {
     box-sizing: border-box;
   }
-`
+`;
 
 export default css`
   div {
@@ -38,7 +37,7 @@ export default css`
   main {
     background: #fff;
     border-radius: 10px;
-    box-shadow: 0 10px 25px rgba(0, 0, 0, .1);
+    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
     height: 100%;
     width: 100%;
   }
@@ -49,4 +48,4 @@ export default css`
       width: ${breakpoints.mobile};
     }
   }
-`
+`;
