d8b515fb19e1e21af3c55f318d618b1ec86b076a
gilsantosjulian <gilsantosjulian@gmail.com>
2021-10-06 19:56:15 -0500

    feat: add Devit component

---
+++ components/Devit/index.js
@@ -0,0 +1,36 @@
+import Avatar from "components/Avatar"
+
+const Devit = ({ avatar, username, message, key }) => {
+  return (
+    <>
+      <article key={key}>
+        <div>
+          <Avatar alt={username} src={avatar} />
+        </div>
+        <section>
+          <strong>{username}</strong>
+          <p>{message}</p>
+        </section>
+      </article>
+
+      <style jsx>{`
+        article {
+          border-bottom: 2px solid #eaf7ff;
+          display: flex;
+          padding: 10px 15px;
+        }
+
+        div {
+          padding-right: 10px;
+        }
+
+        p {
+          line-height: 1.3125;
+          margin: 0;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default Devit
