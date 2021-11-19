6d50f76e3720bf5e065c2a52ee9253cc9a46601f
gilsantosjulian <gilsantosjulian@gmail.com>
2021-11-19 13:41:41 -0500

    feat: get imageUrl and show it

--- pages/compose/tweet/index.js
+++ pages/compose/tweet/index.js
@@ -37,46 +37,70 @@ const ComposeTweet = () => {
 
   useEffect(() => {
     if (task) {
-      // const onProgress = () => {}
-      // const onError = () => {}
-      // eslint-disable-next-line no-unused-vars
-      const onComplete = () => {
-        console.log("onComplete")
-        task.snapshot.ref.getDownloadURL().then(setImgURL)
+      const onProgress = (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        console.log("Upload is " + progress + "% done")
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused")
+            break
+          case "running":
+            console.log("Upload is running")
+            break
+        }
       }
 
-      // task.on("state_changed", onError, onProgress, onComplete)
+      const onError = (error) => {
+        console.log("unsuccesfull upload")
+        console.log({ error })
+      }
+      // eslint-disable-next-line no-unused-vars
+      const onComplete = () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        console.log("onComplete")
+        getDownloadURL(task.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL)
+          setImgURL(downloadURL)
+        })
+      }
 
-      task.on(
-        "state_changed",
-        (snapshot) => {
-          // Observe state change events such as progress, pause, and resume
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          console.log("Upload is " + progress + "% done")
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused")
-              break
-            case "running":
-              console.log("Upload is running")
-              break
-          }
-        },
-        // eslint-disable-next-line node/handle-callback-err
-        (error) => {
-          console.log("unsuccesfull upload")
-        },
-        () => {
-          // Handle successful uploads on complete
-          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-          console.log("onComplete")
-          getDownloadURL(task.snapshot.ref).then((downloadURL) => {
-            console.log("File available at", downloadURL)
-          })
-        }
-      )
+      task.on("state_changed", onProgress, onError, onComplete)
+
+      console.log({ task })
+
+      // task.on(
+      //   "state_changed",
+      //   (snapshot) => {
+      //     // Observe state change events such as progress, pause, and resume
+      //     // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      //     const progress =
+      //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      //     console.log("Upload is " + progress + "% done")
+      //     switch (snapshot.state) {
+      //       case "paused":
+      //         console.log("Upload is paused")
+      //         break
+      //       case "running":
+      //         console.log("Upload is running")
+      //         break
+      //     }
+      //   },
+      //   // eslint-disable-next-line node/handle-callback-err
+      //   (error) => {
+      //     console.log("unsuccesfull upload")
+      //   },
+      //   () => {
+      //     // Handle successful uploads on complete
+      //     // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      //     console.log("onComplete")
+      //     getDownloadURL(task.snapshot.ref).then((downloadURL) => {
+      //       console.log("File available at", downloadURL)
+      //     })
+      //   }
+      // )
     }
   }, [task])
 
@@ -120,7 +144,7 @@ const ComposeTweet = () => {
     setDrag(DRAG_IMAGE_STATES.NONE)
     const file = event.dataTransfer.files[0]
     const task = uploadImage(file)
-    setDrag(task)
+    setTask(task)
   }
 
   const isButtonDisabled = !message.length || status === COMPOSE_STATES.LOADING
@@ -139,6 +163,7 @@ const ComposeTweet = () => {
             onDrop={handleDrop}
             placeholder="¿Qué está pasando?"
           ></textarea>
+          {imageURL && <img src={imageURL} />}
           <div>
             <Button disabled={isButtonDisabled}>Devitear</Button>
           </div>
