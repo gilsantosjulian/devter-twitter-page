b0395fb9f8ec76ec2ad25b350983957d8bb2f039
gilsantosjulian <gilsantosjulian@gmail.com>
2021-11-30 18:47:24 -0500

    fix: remove navigator - api from client inside server

--- hooks/useDateTimeFormat.js
+++ hooks/useDateTimeFormat.js
@@ -2,7 +2,7 @@ import { DEFAULT_LANGUAGE } from "constants/locale"
 
 const useDateTimeFormat = (timestamp) => {
   const date = new Date(timestamp)
-  const language = navigator.language || DEFAULT_LANGUAGE
+  const language = DEFAULT_LANGUAGE
 
   const options = {
     year: "numeric",
