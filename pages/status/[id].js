0ac3f2b10ec9156fa56113b59849ad6b7b003055
gilsantosjulian <gilsantosjulian@gmail.com>
2021-11-30 18:49:58 -0500

    feat: add getInitialProps on DevitPage

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -1,10 +1,21 @@
-const DevitPage = () => {
+import Devit from "components/Devit"
+
+const DevitPage = (props) => {
   return (
     <>
-      We can change it
+      <Devit {...props} />
       <style jsx>{``}</style>
     </>
   )
+}
+
+DevitPage.getInitialProps = (context) => {
+  const { query } = context
+  const { id } = query // id, make relation with the file that was created: status/[id]
+  return fetch(`http://localhost:4000/api/devits/${id}`).then((apiResponse) => {
+    console.log({ apiResponse })
+    if (apiResponse.ok) return apiResponse.json()
+  })
 }
 
 export default DevitPage
