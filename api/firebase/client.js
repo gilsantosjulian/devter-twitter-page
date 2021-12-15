22743f6867afc9ba09781db7803ad3a76f77e0bb
gilsantosjulian <gilsantosjulian@gmail.com>
2021-12-14 22:01:17 -0500

    feat: get firebase config env var

--- api/firebase/client.js
+++ api/firebase/client.js
@@ -22,15 +22,7 @@ import { getStorage, ref, uploadBytesResumable } from "firebase/storage"
 const githubProvider = new GithubAuthProvider()
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBCwOUP6P8L1xeEMWzzrGuwGrxTQ59QtYs",
-  authDomain: "devter-c4676.firebaseapp.com",
-  projectId: "devter-c4676",
-  storageBucket: "devter-c4676.appspot.com",
-  messagingSenderId: "994583765336",
-  appId: "1:994583765336:web:0f1587c7d0972a9db0852d",
-  measurementId: "G-VNV8P4YJC4",
-}
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
 
 !firebase.getApps.length && firebase.initializeApp(firebaseConfig)
 
